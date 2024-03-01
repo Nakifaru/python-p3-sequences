e88c8cae35ef3c9d231f20abd8d96435f1c79a9b
Michael <michaelmwendwamumo@gmail.com>
2024-03-01 13:13:30 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    series = [0, 1]
+
+    if length == 0:
+        print([])
+    
+    elif length == 1:
+        print([0])
+    
+    elif length == 2:
+        print (series)
+    
+    else:
+        last_num = series[-1]
+        second_last = series[-2]
+        i = 3
+        while i <= length:
+            series.append(last_num + second_last)
+            last_num = series[-1]
+            second_last = series[-2]
+            i += 1
+        print(series)
     pass
